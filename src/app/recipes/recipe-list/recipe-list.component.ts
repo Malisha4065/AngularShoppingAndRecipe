74bcb747dd58765aef7e062900d10087f11b95f1
Malisha A.P.D <128911037+Malisha4065@users.noreply.github.com>
2023-11-18 16:55:35 +0530

    recipe component improved

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+
+import {Recipe} from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrl: './recipe-list.component.css'
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test',
+      'https://www.century-foods.com/wp-content/uploads/2023/01/px653047-image-kwvxn16x.jpg'),
+    new Recipe('A Test Recipe', 'This is simply a test',
+      'https://www.century-foods.com/wp-content/uploads/2023/01/px653047-image-kwvxn16x.jpg')
+  ];
 }
